31de4fc67aa99e456a1683baf017955872863a7a
Ilia Romanov <iliaromanov033@gmail.com>
2021-02-10 12:57:44 -0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -15,7 +15,6 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
-            print("NO USER ID IN SESSION")
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
