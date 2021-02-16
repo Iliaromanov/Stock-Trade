b577371ab0425a422016e8a9d7bf9c6a62500e68
Ilia Romanov <iliaromanov033@gmail.com>
2021-02-15 20:09:34 -0500

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-
-
-
-# Temporary fix for demo (will use env variables later)
-DB_NAME = "ddbsl7wcmixccheyip3odvh"
-DB_HOST = "ec2-3-90-70-174.compute-1.amazonaws.com"
-DB_USER = "uykaaeu71ib6"
-DB_PASS = "pnnjxxkx01aqnw6n61mm7coxtzsbefnxb"
