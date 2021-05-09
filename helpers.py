c620e769386e365365fa6b9989de674c5a417007
Iliaromanov <iliaromanov033@gmail.com>
2021-05-09 17:09:26 -0400

    iex api url change

--- helpers.py
+++ helpers.py
@@ -39,7 +39,7 @@ def lookup(symbol: str) -> dict:
 
     # Contact API
     try:
-        api_key = os.environ.get("API_KEY")
+        api_key = "pk_416fe4a246914cb594e7deeda6251bf5" #os.environ.get("API_KEY")
         response = requests.get(f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
         response.raise_for_status()
     except requests.RequestException:
