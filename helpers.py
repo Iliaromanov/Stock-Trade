a934b00b9780930b5b9c0e783b19c073e887a3bc
Iliaromanov <iliaromanov033@gmail.com>
2021-05-09 17:06:25 -0400

    iex api url change

--- helpers.py
+++ helpers.py
@@ -40,7 +40,7 @@ def lookup(symbol: str) -> dict:
     # Contact API
     try:
         api_key = os.environ.get("API_KEY")
-        response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
+        response = requests.get(f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
         response.raise_for_status()
     except requests.RequestException:
         flash("IEX is currently under maintenance. Sorry for the inconvenience", "error")
